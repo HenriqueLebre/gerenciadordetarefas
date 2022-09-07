77f25dcc63bd361279d3d4fce7894e03491609e9
HenriqueLebre <lebre.henrique@gmail.com>
2022-09-07 11:50:10 -0400

    aplicativo de gerenciador de tarefas com temporizador, usando Vue3

---
+++ src/components/Box.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({ /* eslint-disable */
+    name: 'Box'
+})
+</script>
+<style scoped>
+.box { 
+    background: #FAF0CA;
+    }
+</style>
